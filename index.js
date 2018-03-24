eab729fc1f3878d5ffd12c1cef1cf111b0cf3f70
MoonTahoe <alex@moonhighway.com>
2018-03-24 16:59:21 -0700

    Structure, Move Resolvers

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const { GraphQLServer } = require('graphql-yoga')
 const fs = require('fs')
 
+const resolvers = require('./resolvers')
 const typeDefs = fs.readFileSync('./typeDefs.graphql', 'UTF-8')
-
-const resolvers = {
-    Query: {
-        gnar() {
-            return 'gnarly!!!'
-        }
-    }
-}
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 
---
+++ resolvers/Query.js
@@ -0,0 +1,5 @@
+module.exports = {
+    gnar() {
+        return 'gnarly!!!'
+    }
+}
