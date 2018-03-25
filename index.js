3c9109ed274f7a4aa3ac77dd5e809f00c90f027e
MoonTahoe <alex@moonhighway.com>
2018-03-24 17:01:45 -0700

    Extend Express, Add Home Route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const typeDefs = fs.readFileSync('./typeDefs.graphql', 'UTF-8')
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 
+server.express.get('/', (req, res) => {
+    res.end('Welcome to the PhotoShare API')
+})
+
 server.start(
     () => console.log(`graph service running - http://localhost:4000`)
 )
