2d340600124054deebd5ecc11b3dc619832af76b
MoonTahoe <alex@moonhighway.com>
2018-03-24 18:54:51 -0700

    Finding Users Tagged in a Photo

--- resolvers/Photo.js
+++ resolvers/Photo.js
@@ -2,5 +2,10 @@ const { findBy } = require('../lib')
 
 module.exports = {
     url: root => `/img/photos/${root.id}.jpg`,
-    postedBy: (root, args, { users }) => findBy(root.userID, users)
+    postedBy: (root, args, { users }) => findBy(root.userID, users),
+    taggedUsers: (root, args, { tags, users }) => tags 
+        .filter(tag => tag.photoID === root.id)  
+        .map(tag => tag.userID)
+        .map(userID => users.map(x=>x.id).indexOf(userID))
+        .map(index => users[index])
 }
