1349ad0337297f6cdc7b7d91b9d696a232c670fb
MoonTahoe <alex@moonhighway.com>
2018-03-24 18:08:30 -0700

    Listing All Users

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -3,5 +3,7 @@ const { findBy } = require('../lib')
 module.exports = {
     totalPhotos: (root, args, ctx) => ctx.photos.length,
     allPhotos: (root, args, ctx) => ctx.photos,
-    Photo: (root, args, ctx) => findBy(args.id, ctx.photos)
+    Photo: (root, args, ctx) => findBy(args.id, ctx.photos),
+    totalUsers: (root, args, ctx) => ctx.users.length,
+    allUsers: (root, args, ctx) => ctx.users
 }
