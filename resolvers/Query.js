30e03cf34d115958fb032d2609276a5a64c06d67
MoonTahoe <alex@moonhighway.com>
2018-03-24 17:54:40 -0700

    Querying a Single Photo

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,4 +1,7 @@
+const { findBy } = require('../lib')
+
 module.exports = {
     totalPhotos: (root, args, ctx) => ctx.photos.length,
-    allPhotos: (root, args, ctx) => ctx.photos
+    allPhotos: (root, args, ctx) => ctx.photos,
+    Photo: (root, args, ctx) => findBy(args.id, ctx.photos)
 }
