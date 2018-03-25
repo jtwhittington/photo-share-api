404f6c948ddc783af65487953cfa94ddccafc2b2
MoonTahoe <alex@moonhighway.com>
2018-03-24 18:58:23 -0700

    Finding All Photos where a User has Been Tagged

--- resolvers/User.js
+++ resolvers/User.js
@@ -1,5 +1,10 @@
 const { findBy } = require('../lib')
 
 module.exports = {
-    postedPhotos: ({id}, args, {photos}) => photos.filter(p => p.userID === id)
+    postedPhotos: ({id}, args, {photos}) => photos.filter(p => p.userID === id),
+    inPhotos: (root, args, { tags, photos }) => tags
+        .filter(tag => tag.userID === root.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.map(x=>x.id).indexOf(photoID))
+        .map(index => photos[index])
 }
