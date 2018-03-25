033bd6b0176523945d2179dbe8dae9d001c335fd
MoonTahoe <alex@moonhighway.com>
2018-03-24 18:24:00 -0700

    Adding postedBy Connection

--- resolvers/Photo.js
+++ resolvers/Photo.js
@@ -1,3 +1,6 @@
+const { findBy } = require('../lib')
+
 module.exports = {
-    url: root => `/img/photos/${root.id}.jpg`
+    url: root => `/img/photos/${root.id}.jpg`,
+    postedBy: (root, args, { users }) => findBy(root.userID, users)
 }
